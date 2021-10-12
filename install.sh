a45c1eba94d2b29aca3fcdea3523ed720189dead
Markus Andersson <markus@enlitenide.se>
2021-10-12 08:20:10 +0200

    Clean up download_stage3

--- install.sh
+++ install.sh
@@ -12,9 +12,7 @@ download_stage3() {
   printf "\n"
   echo -e "\e[33m\xe2\x8f\xb3 Downloading the stage 3 tarball... \e[m"
   LATEST=$(wget --quiet $MIRROR/releases/amd64/autobuilds/latest-stage3-amd64.txt -O- | tail -n 1 | cut -d " " -f 1)
-  echo $LATEST
 
-  BASENAME=$(basename "$LATEST")
   wget -q --show-progress "$MIRROR/releases/amd64/autobuilds/$LATEST" 
 }
 
